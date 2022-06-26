68b919d6646816f1b118d6ecf339338fca438b38
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-06-26 13:32:56 +0300

    Update container

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,20 +1,9 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
-export const GlobalContainer = styled.div` 
-    width: 1100px;
-    padding: 0 20px;
-    margin: 0 auto;
-    background-color: rgb(197, 185, 185);
-    border: 2px solid black;
-    /* text-align: center; */
+export const GlobalContainer = styled.div`
+  width: 1100px;
+  padding: 0 20px;
+  margin: 0 auto;
+  background-color: rgb(197, 185, 185);
+  border: 2px solid black;
 `;
-
-
-
-// .container{
-    // width: 1100px;
-    // padding: 0 20px;
-    // margin: 0 auto;
-    // background-color: rgb(197, 185, 185);
-// }
-
